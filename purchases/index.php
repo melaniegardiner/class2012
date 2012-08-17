ff915de5988a75617f1f85756c2ad32dace9f5a9
melaniegardiner <mela_knee@hotmail.com>
2012-08-17 11:57:25 -0400

    put db in code

--- purchases/index.php
+++ purchases/index.php
@@ -1,2 +1,18 @@
+<?php 
+
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/purchases',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
+?>
 
 <html><body>hello world </body?</html>
---
+++ purchases/model/Purchase.php
@@ -0,0 +1,6 @@
+<?php
+
+class Purchase extends ActiveRecord\Model{
+
+}
+?>
