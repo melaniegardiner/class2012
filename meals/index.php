e978ff82571b601c9653937dddcbc781c063aa71
melaniegardiner <mela_knee@hotmail.com>
2012-08-20 09:45:21 -0400

    added Meal.php class file and updated index file

--- meals/index.php
+++ meals/index.php
@@ -1,1 +1,19 @@
 <html><body>Hullo world!</body></html>
+
+<?php 
+
+require '../ActiveRecord/ActiveRecord.php';
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+	$cfg->set_model_directory('model');
+	$cfg->set_connections(
+			array(
+					'development' => 'mysql://root:@localhost/meals',
+					'test' => 'mysql://username:password@localhost/test_database_name',
+					'production' => 'mysql://username:password@localhost/production_database_name'
+			)
+	);
+});
+
+?>
