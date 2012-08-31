555cde65359fa6923edae8ae98dee5efc6694361
melaniegardiner <mela_knee@hotmail.com>
2012-08-31 09:42:29 -0400

    updated index to scrape data from wowjobs.ca

--- nextaction/index.php
+++ nextaction/index.php
@@ -1,4 +1,31 @@
 <?php
+//********************
+//scrape data from website
+//***************************
+	
+	header("Content-Type: text/plain");
+	
+	//wowjobs
+	$sIn = file_get_contents("http://www.wowjobs.ca/wowrss.aspx?q=Web+Developer&1=N2T1G8&s=sr=25&t=30&f=r&e=si=A&Dup=H");
+	
+	
+	
+	echo $sIn;
+	//exit();
+	
+	if(preg_match_all("|<title(.*)</title>|U", $sIn, $aIn))
+	{
+		//print_r($aIn);
+		foreach($aIn[0] as $sTitle)
+		{
+			echo $sTitle ."\n";
+		}//end foreach
+	}//end if
+	else 
+	{
+		echo "nothing to display";	
+	}//end else
+
 ?>
 
 <html><body>hullo worldz!</body></html>
